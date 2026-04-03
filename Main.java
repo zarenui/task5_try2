878410306e38dfca4128bcb68858e48c14711874
zarenui <konrolz6337@gmail.com>
2026-04-03 16:09:53 +0300

    Добавление цикла

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ package task5_try2;
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        for (int i = 0; i<10; i++){
+            System.out.println(i);
+        }
     }
 }
