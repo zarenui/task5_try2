b094f9bbbe93f89e8f23b023356e8d09567a238f
zarenui <konrolz6337@gmail.com>
2026-04-03 16:13:45 +0300

    Добавление словаря

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 package task5_try2;
 
+import java.util.HashMap;
+
 class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         for (int i = 0; i<10; i++){
             System.out.println(i);
         }
+        HashMap<Integer, String> romenum = new HashMap<>();
+        romenum.put(1, "I");
+        romenum.put(2,"II");
     }
 }
